c5359b4734ce89a1425ccfb9c2340c0080d5b7d4
weeger <romain.weeger@gmail.com>
2023-09-25 08:01:08 +0200

    Export

---
+++ src/FormProcessor/Traits/PricedFormProcessorTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wexample\SymfonyMoney\FormProcessor\Traits;
+
+use Symfony\Component\Form\FormInterface;
+use Wexample\SymfonyHelpers\Entity\Interfaces\AbstractEntityInterface;
+use Wexample\SymfonyHelpers\Service\Entity\EntityNeutralService;
+
+trait PricedFormProcessorTrait
+{
+    protected function pricedOnSubmitted(
+        AbstractEntityInterface $entity,
+        FormInterface $form
+    ) {
+        $fieldSwitch = $form->has('priceTotalOverrideSwitch') ? $form->get('priceTotalOverrideSwitch') : null;
+
+        if (!$fieldSwitch || !$fieldSwitch->getData()) {
+            $entity->setPriceOverridden(null);
+        }
+
+        // Save computed price.
+        $this->getEntityService()->pricedEntityUpdate($entity);
+    }
+
+    abstract protected function getEntityService(): EntityNeutralService;
+}
