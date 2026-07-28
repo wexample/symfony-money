3ca376de30c6754fe100b6222dfe388e8d660049
weeger <romain.weeger@gmail.com>
2026-07-28 14:23:30 +0200

    Cleanup

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wexample\SymfonyMoney\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Wexample\SymfonyApi\Api\Class\ApiResponse;
+use Wexample\SymfonyApi\Api\Controller\AbstractApiController;
+use Wexample\SymfonyMoney\Service\CurrencyService;
+
+#[Route(path: '_money/currency/', name: '_money_currency_')]
+class CurrencyController extends AbstractApiController
+{
+    final public const ROUTE_IMPORT = 'import';
+
+    #[Route(path: 'import', name: self::ROUTE_IMPORT, methods: [Request::METHOD_POST])]
+    public function import(CurrencyService $currencyService): ApiResponse
+    {
+        $currencyService->seed();
+
+        return self::apiResponseSuccess();
+    }
+}
