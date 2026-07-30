fa9ef10a1ac3d36532b99f42aebbfbc26132e09f
weeger <romain.weeger@gmail.com>
2026-07-30 12:49:22 +0200

    Cleanup

--- src/Api/Normalizer/Entity/Currency/DefaultCurrencyNormalizer.php
+++ src/Api/Normalizer/Entity/Currency/DefaultCurrencyNormalizer.php
@@ -4,7 +4,9 @@ namespace Wexample\SymfonyMoney\Api\Normalizer\Entity\Currency;
 
 use ArrayObject;
 use Wexample\SymfonyHelpers\Entity\AbstractEntity;
+use Wexample\SymfonyHelpers\Interface\NormalizableDataInterface;
 use Wexample\SymfonyHelpers\Normalizer\AbstractEntityNormalizer;
+use Wexample\SymfonyMoney\Api\Dto\PublicCurrencyDto;
 use Wexample\SymfonyMoney\Entity\Currency;
 use Wexample\SymfonyMoney\Entity\Traits\Manipulator\CurrencyEntityManipulatorTrait;
 
@@ -16,14 +18,7 @@ class DefaultCurrencyNormalizer extends AbstractEntityNormalizer
         Currency|AbstractEntity $entity,
         ?string $format = null,
         array $context = []
-    ): array|string|int|float|bool|ArrayObject|null {
-        return parent::normalizeEntity($entity, $format, $context)
-            + [
-                'currencyCode' => $entity->getCurrencyCode(),
-                'name' => $entity->getName(),
-                'type' => $entity->getType(),
-                'currencySymbol' => $entity->getCurrencySymbol(),
-                'decimals' => $entity->getDecimals(),
-            ];
+    ): array|string|int|float|bool|ArrayObject|NormalizableDataInterface|null {
+        return PublicCurrencyDto::fromEntity($entity);
     }
 }
