51f52b93dab89be9f04125c992cbb3fe98c869cb
weeger <romain.weeger@gmail.com>
2026-07-30 10:01:03 +0200

    Cleanup

--- src/WexampleSymfonyMoneyBundle.php
+++ src/WexampleSymfonyMoneyBundle.php
@@ -3,12 +3,18 @@
 namespace Wexample\SymfonyMoney;
 
 use Wexample\SymfonyHelpers\Class\AbstractBundle;
+use Wexample\SymfonyHelpers\Interface\LoaderBundleInterface;
 use Wexample\SymfonyPseudocode\Interface\PseudocodeBundleInterface;
 
-class WexampleSymfonyMoneyBundle extends AbstractBundle implements PseudocodeBundleInterface
+class WexampleSymfonyMoneyBundle extends AbstractBundle implements PseudocodeBundleInterface, LoaderBundleInterface
 {
     public static function getPseudocodeSourcePaths(): array
     {
         return [__DIR__ . '/'];
+    }
+
+    public static function getLoaderFrontPaths(): array
+    {
+        return [__DIR__ . '/../front/'];
     }
 }
